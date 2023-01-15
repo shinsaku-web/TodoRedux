496c4a64f80bf1bd31767267e9ac201e3ac9c510
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2023-01-15 10:10:22 +0900

    update:Todoの個別表示

--- src/components/templates/TodoTemplate.tsx
+++ src/components/templates/TodoTemplate.tsx
@@ -1,18 +1,36 @@
-import { Box, Heading } from '@chakra-ui/react'
+import { useTodos } from '@/hooks/useTodos'
+import { Box, Button, Heading, Text } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 
 export const TodoTemplate = () => {
   const router = useRouter()
   const { id } = router.query
-  // TODO: todoを引数で指定して取ってくる関数を作成しimport
-  // const res = useSelector((state: { todos: { todos: Todo[] } }) => state.todos.todos)
+  const { useGetTodo } = useTodos()
+  const todo = useGetTodo(Number(id))
+
+  const handleTopButton = () => {
+    router.push('/')
+  }
+
+  const handleUpdateButton = (id: number) => {
+    router.push(`/update/${id}`)
+  }
 
   return (
     <Box>
       <Heading as='h1' size='xl' color={'blue.400'} textAlign={'center'}>
         TodoTemplate
       </Heading>
-      <Box pt={6}>{id}</Box>
+      <Box pt={6} display='flex' justifyContent={'space-around'}>
+        <Button onClick={handleTopButton}>Go To Top</Button>
+        <Button onClick={() => handleUpdateButton(Number(id))}>Update Todo</Button>
+      </Box>
+      <Box pt={10}>
+        <Heading as={'h2'} size={'md'}>
+          {todo.title}
+        </Heading>
+        <Text mt={4}>{todo.content}</Text>
+      </Box>
     </Box>
   )
 }
