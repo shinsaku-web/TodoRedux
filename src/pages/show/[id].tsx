8a712594e557e191e3229c17d71ad1461ad76233
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2023-01-30 20:18:44 +0900

    fix:propsを埋める

--- src/pages/show/[id].tsx
+++ src/pages/show/[id].tsx
@@ -1,5 +1,8 @@
 import { TodoTemplate } from '@/components/templates/TodoTemplate'
 
-export default function Show() {
+export default function Show(props: any) {
   return <TodoTemplate />
+}
+export async function getServerSideProps() {
+  return { props: undefined }
 }
--- src/pages/update/[id].tsx
+++ src/pages/update/[id].tsx
@@ -1,5 +1,9 @@
 import { UpdateTodoTemplate } from '@/components/templates/UpdateTodoTemplate'
 
-export default function Update() {
+export default function Update(props: any) {
   return <UpdateTodoTemplate />
+}
+
+export async function getServerSideProps() {
+  return { props: undefined }
 }
